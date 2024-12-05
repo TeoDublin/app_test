29bb0c25e4b9431bd7a78186190535a2c55802e2
teo <developer2.it@integraa.it>
2024-12-05 08:54:21 +0100

    first commit

---
+++ app/src/main/java/com/example/app_test_jdk17_android_7_api24/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.app_test_jdk17_android_7_api24.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/app_test_jdk17_android_7_api24/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.app_test_jdk17_android_7_api24.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
